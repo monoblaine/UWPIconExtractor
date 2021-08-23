970c1cb9c32470021d3390630c63bfb6380dc3db
Serhan Apaydın <zerhan@gmail.com>
2021-08-23 17:35:41 +0300

    Not ekle

--- UWPIconExtractor/Main.cs
+++ UWPIconExtractor/Main.cs
@@ -9,6 +9,13 @@ namespace UWPIconExtractor {
         [DllExport("getFileName", CallingConvention = CallingConvention.Cdecl)]
         public static String GetFileName (IntPtr id) {
             var package = AppxPackage.FromWindow(id);
+
+            /*
+             * TODO: Sonradan sıçış olursa şurayı incele:
+             * https://stackoverflow.com/questions/37417757/extract-icon-from-uwp-application
+             * https://stackoverflow.com/questions/37686916/how-do-i-retrieve-a-windows-store-apps-icon-from-a-c-sharp-desktop-app
+             */
+
             var pathToImage = Path.Combine(
                 package.Path,
                 package.Apps[0].Square44x44Logo
