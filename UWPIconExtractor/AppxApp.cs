5cfe488dea7ddc9c4235502cc34ddbd8d24bb3f3
Serhan Apaydın <zerhan@gmail.com>
2021-08-23 17:57:05 +0300

    İşe yaramayan kodları sil

--- UWPIconExtractor/AppxApp.cs
+++ UWPIconExtractor/AppxApp.cs
@@ -19,42 +19,11 @@ namespace UWPIconExtractor {
 
         // we code well-known but there are others (like Square71x71Logo, Square44x44Logo, whatever ...)
         // https://msdn.microsoft.com/en-us/library/windows/desktop/hh446703.aspx
-        public String Description { get; internal set; }
-
-        public String DisplayName { get; internal set; }
-
-        public String EntryPoint { get; internal set; }
-
-        public String Executable { get; internal set; }
 
         public String Id { get; internal set; }
 
         public String Logo { get; internal set; }
 
-        public String SmallLogo { get; internal set; }
-
-        public String StartPage { get; internal set; }
-
-        public String Square150x150Logo { get; internal set; }
-
-        public String Square30x30Logo { get; internal set; }
-
         public String Square44x44Logo { get; internal set; }
-
-        public String BackgroundColor { get; internal set; }
-
-        public String ForegroundText { get; internal set; }
-
-        public String WideLogo { get; internal set; }
-
-        public String Wide310x310Logo { get; internal set; }
-
-        public String ShortName { get; internal set; }
-
-        public String Square310x310Logo { get; internal set; }
-
-        public String Square70x70Logo { get; internal set; }
-
-        public String MinWidth { get; internal set; }
     }
 }
--- UWPIconExtractor/AppxPackage.cs
+++ UWPIconExtractor/AppxPackage.cs
@@ -21,29 +21,9 @@ namespace UWPIconExtractor {
 
         public String Path { get; private set; }
 
-        public String Publisher { get; private set; }
-
-        public String PublisherId { get; private set; }
-
-        public String ResourceId { get; private set; }
-
-        public String FamilyName { get; private set; }
-
         public String ApplicationUserModelId { get; private set; }
 
         public String Logo { get; private set; }
-
-        public String PublisherDisplayName { get; private set; }
-
-        public String Description { get; private set; }
-
-        public String DisplayName { get; private set; }
-
-        public Boolean IsFramework { get; private set; }
-
-        public Version Version { get; private set; }
-
-        public AppxPackageArchitecture ProcessorArchitecture { get; private set; }
 
         public IReadOnlyList<AppxApp> Apps {
             get {
@@ -162,14 +142,8 @@ namespace UWPIconExtractor {
                 for (var i = 0; i < count; i++) {
                     var info = (PACKAGE_INFO) Marshal.PtrToStructure(infoBuffer + i * Marshal.SizeOf(typeof(PACKAGE_INFO)), typeof(PACKAGE_INFO));
                     var package = new AppxPackage {
-                        FamilyName = Marshal.PtrToStringUni(info.packageFamilyName),
                         FullName = Marshal.PtrToStringUni(info.packageFullName),
                         Path = Marshal.PtrToStringUni(info.path),
-                        Publisher = Marshal.PtrToStringUni(info.packageId.publisher),
-                        PublisherId = Marshal.PtrToStringUni(info.packageId.publisherId),
-                        ResourceId = Marshal.PtrToStringUni(info.packageId.resourceId),
-                        ProcessorArchitecture = info.packageId.processorArchitecture,
-                        Version = new Version(info.packageId.VersionMajor, info.packageId.VersionMinor, info.packageId.VersionBuild, info.packageId.VersionRevision)
                     };
 
                     // read manifest
@@ -185,36 +159,16 @@ namespace UWPIconExtractor {
                     var reader = factory.CreateManifestReader(strm);
 
                     package._properties = reader.GetProperties();
-                    package.Description = package.GetPropertyStringValue("Description");
-                    package.DisplayName = package.GetPropertyStringValue("DisplayName");
                     package.Logo = package.GetPropertyStringValue("Logo");
-                    package.PublisherDisplayName = package.GetPropertyStringValue("PublisherDisplayName");
-                    package.IsFramework = package.GetPropertyBoolValue("Framework");
 
                     var apps = reader.GetApplications();
 
                     while (apps.GetHasCurrent()) {
                         var app = apps.GetCurrent();
                         var appx = new AppxApp(app) {
-                            Description = GetStringValue(app, "Description"),
-                            DisplayName = GetStringValue(app, "DisplayName"),
-                            EntryPoint = GetStringValue(app, "EntryPoint"),
-                            Executable = GetStringValue(app, "Executable"),
                             Id = GetStringValue(app, "Id"),
                             Logo = GetStringValue(app, "Logo"),
-                            SmallLogo = GetStringValue(app, "SmallLogo"),
-                            StartPage = GetStringValue(app, "StartPage"),
-                            Square150x150Logo = GetStringValue(app, "Square150x150Logo"),
-                            Square30x30Logo = GetStringValue(app, "Square30x30Logo"),
-                            Square44x44Logo = GetStringValue(app, "Square44x44Logo"),
-                            BackgroundColor = GetStringValue(app, "BackgroundColor"),
-                            ForegroundText = GetStringValue(app, "ForegroundText"),
-                            WideLogo = GetStringValue(app, "WideLogo"),
-                            Wide310x310Logo = GetStringValue(app, "Wide310x310Logo"),
-                            ShortName = GetStringValue(app, "ShortName"),
-                            Square310x310Logo = GetStringValue(app, "Square310x310Logo"),
-                            Square70x70Logo = GetStringValue(app, "Square70x70Logo"),
-                            MinWidth = GetStringValue(app, "MinWidth")
+                            Square44x44Logo = GetStringValue(app, "Square44x44Logo")
                         };
 
                         package._apps.Add(appx);
